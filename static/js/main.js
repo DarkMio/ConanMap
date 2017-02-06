87d8c90e3961a5b1a5de6cb7f5dce032fd634405
Mio Bambino <s59330@beuth-hochschule.de>
2017-02-06 15:02:42 +0100

    tile layering

--- static/js/main.js
+++ static/js/main.js
@@ -4,14 +4,36 @@
 
 document.addEventListener('DOMContentLoaded', function() {
 
-    var map = L.map('mapid', { crs: L.CRS.Simple, minZoom: 0 });
+    var map_scale = function() {
+        var min = [-1088, 130];
+        var max = [-135, 1944];
+        return function(x, y) {
+            return [min[0] + (max[0] - min[0]) * (1 - x), min[1] + (max[1] - min[1]) * y];
+        };
+    }();
+
+
+
+
+    var map = L.map('mapid', { crs: L.CRS.Simple, minZoom: -5 , center: [0.0, 0.0]});
 
     // the map is 3627 x 1920 big - this means:
     // 1 px is 0.3 map units
     var bounds = [[0, 0], [640, 1209]];
-    var image = L.imageOverlay('static/tiles/__map.png', bounds).addTo(map);
+    // var image = L.imageOverlay('static/tiles/__map.png', bounds).addTo(map);
 
-    map.fitBounds(bounds);
+    map.fitBounds([[-1088, 130], [-135, 1944]]);
+
+    L.tileLayer('static/tiles/{z}_{x}_{y}.png', {
+        // attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+        noWrap: true,
+        minZoom: -5,
+        maxZoom: 7,
+        zoomOffset: 6,
+        bounds: [[-135, 130], [-1088, 1944]]
+    }).addTo(map);
+
+    map.setView(map_scale(0.5, 0.5), -1);
 
     var greenIcon = L.icon({
         iconUrl: 'static/img/priest_marker_small.png',
@@ -29,9 +51,14 @@ document.addEventListener('DOMContentLoaded', function() {
     L.marker([403, 988], {icon: greenIcon}).addTo(map).bindPopup("Teacher of Mitra");
 
 
+    L.marker([0, 0]).addTo(map);
 
     map.on('click', function(e) {
         console.log(e.latlng);
-    })
+    });
+
+    for(var i = 0; i < 1; i += 0.1) {
+        L.marker(map_scale(i, i)).addTo(map);
+    }
 
 });
